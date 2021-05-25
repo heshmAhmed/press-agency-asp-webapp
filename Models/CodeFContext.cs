d147c6350be6a395959269e494168f6e72cff65d
heshmAhmed <heshamahmed22226.ha@gmail.com>
2021-05-25 13:22:07 +0200

    add connection string and migrations

---
+++ Models/CodeFContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace press_agency_asp_webapp.Models
+{
+    public class CodeFContext : DbContext
+    {
+        public CodeFContext()
+            : base("Name=DefaultConnection")
+        {
+        
+        }
+
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Editor> Editors { get; set; }
+        public DbSet<Viewer> Viewers { get; set; }
+    }
+}
---
+++ Models/Viewer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace press_agency_asp_webapp.Models
+{
+    public class Viewer : Actor 
+    {
+
+    }
+}
